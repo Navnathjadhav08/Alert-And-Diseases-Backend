37001b1219b2787d35b789f136c626298e827fa8
Navnath Jadhav <131193155+Navnathjadhav08@users.noreply.github.com>
2025-02-08 14:18:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require("dotenv").config();
 
-const cors = require('cors');
+
 
 app.use(cors({
     origin: '*', // Allow all origins
